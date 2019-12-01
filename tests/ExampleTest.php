c798f40cd711af1e3dd626930980deb8813662c3
Owen Conti <owen@ohseemedia.com>
2019-12-01 15:50:37 -0700

    Fill example files.

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_runs_tests()
+    {
+        $this->assertTrue(true);
+    }
+}
